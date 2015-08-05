de26eeb371380d9ed3dbdecf2c19a3b5d8e4c6f2
Bernard Delhez <bernarddelhez@gmail.com>
2015-08-05 16:48:24 +0200

    Invoice Address is alterable after sale order confirmation

--- model/sale_order.py
+++ model/sale_order.py
@@ -5,6 +5,8 @@ from datetime import date
 class sale_order_improvements(models.Model):
     _inherit = ['sale.order']
     
+    partner_invoice_id = fields.Many2one('res.partner', readonly=False)
+    
     def action_button_confirm(self, cr, uid, ids, context=None):
         super(sale_order_improvements, self).action_button_confirm(cr, uid, ids, context=context)
         self.pool.get('sale.order').write(cr, uid, ids, {'date_order': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),})
