c62433ff9ddad9f1a2bbe80a33dcb46abef57f6b
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-22 11:55:09 +0200

    remove method action_button_confirm (not in v9)

--- __openerp__.py
+++ __openerp__.py
@@ -1,20 +1,20 @@
 {
     'name': "Sale order improvements",
-    'version': '9.0.1.0',
+    'version': '9.0.1.1',
     'depends': ['sale','account_reports_followup'],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Sale',
     'description': 
-    """
-    Sale order improvements.
-        - It set the current date to the order date of quotations or sales orders.
-        - Invoice Address is alterable after sale order confirmation.
-        - New field special condition. Will be included at the end of the quotation report.
-        - Attachment field in Quotations and Sale Orders
-        - It creates a field 'header_html' that will be used with a WYSIWYG and printed on SO reports
+    """Sale order improvements.
 
-	This module has been developed by Bernard DELHEZ @ AbAKUS it-solution under the control of Valentin THIRION.
+    - It set the current date to the order date of quotations or sales orders.
+    - Invoice Address is alterable after sale order confirmation.
+    - New field special condition. Will be included at the end of the quotation report.
+    - Attachment field in Quotations and Sale Orders
+    - It creates a field 'header_html' that will be used with a WYSIWYG and printed on SO reports
+
+This module has been developed by Bernard DELHEZ @ AbAKUS it-solution under the control of Valentin THIRION.
     """,
     'data': ['view/report_saleorder.xml',
              'view/sale_order_view.xml',
--- model/sale_order.py
+++ model/sale_order.py
@@ -1,18 +1,11 @@
 from openerp import models, fields
-import datetime
-from datetime import date
+from datetime import datetime
 
 class sale_order_improvements(models.Model):
-    _inherit = ['sale.order']
+    _inherit = 'sale.order'
     
     partner_invoice_id = fields.Many2one('res.partner', readonly=False)
     special_condition_id = fields.Many2one(comodel_name='sale.order.special.condition', string="Special condition")
     attachments_ids = fields.Many2many('ir.attachment', string="Attachments")
     header_text = fields.Html(string="Optional header text")
     note = fields.Html("Terms and conditions")
-
-    # Set the current date as the date of the quotation        
-    def action_button_confirm(self, cr, uid, ids, context=None):
-        super(sale_order_improvements, self).action_button_confirm(cr, uid, ids, context=context)
-        self.pool.get('sale.order').write(cr, uid, ids, {'date_order': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),})
-        return True
